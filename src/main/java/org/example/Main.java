d4074e73c2ac1cfc5912970f6df9d85190fa8246
JoHyungJun <jhj8371@naver.com>
2023-12-28 02:24:48 +0900

    [#1] 커밋 테스트 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,8 @@ import com.sun.net.httpserver.HttpExchange;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("commit test 22222");
     }
     HttpExchange exchange;
 }
